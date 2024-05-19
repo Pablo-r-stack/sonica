5b2d4cbc84ff39c02bc57768f5e8e36a8ed3965d
Pablo <119269816+Pablo-r-stack@users.noreply.github.com>
2024-05-19 11:35:41 -0300

    Dev pablo (#41)
    * se agrega archivo readme.md
    
    * se agregan fotos dev, se optimiza codigo de indexjs y acercadejs, se arregla banner
    
    * se agrega estilo a fuente de carta evento, se agrega evento de carga a evento.js, se agrega alert a boton comprar entrada

--- Js/evento.js
+++ Js/evento.js
@@ -1,13 +1,19 @@
- // Recuperar los datos del local storage
- const selectedShow = JSON.parse(localStorage.getItem('selectedShow'));
+// Recuperar los datos del local storage
+const selectedShow = JSON.parse(localStorage.getItem('selectedShow'));
+const btnCompra = document.querySelector('#btn-compra');
+// Verificar si hay datos disponibles
+document.addEventListener('DOMContentLoaded', () => {
+    if (selectedShow) {
+        document.querySelector('.banda-evento').src = selectedShow.imagenSrc;
+        document.querySelector('.titulo-evento').textContent = selectedShow.nombre;
+        document.querySelector('.lugar-evento').textContent = `Lugar: ${selectedShow.lugar}`;
+        document.querySelector('.fecha-evento').textContent = `Fecha: ${selectedShow.fecha}`;
+    } else {
+        // Manejar el caso en que no hay datos disponibles
+        document.querySelector('.contenedor-evento').innerHTML = '<p>No hay información disponible para este evento.</p>';
+    }
+})
 
- // Verificar si hay datos disponibles
- if (selectedShow) {
-     document.querySelector('.banda-evento').src = selectedShow.imagenSrc;
-     document.querySelector('.titulo-evento').textContent = selectedShow.nombre;
-     document.querySelector('.lugar-evento').textContent = `Lugar: ${selectedShow.lugar}`;
-     document.querySelector('.fecha-evento').textContent = `Fecha: ${selectedShow.fecha}`;
- } else {
-     // Manejar el caso en que no hay datos disponibles
-     document.querySelector('.contenedor-evento').innerHTML = '<p>No hay información disponible para este evento.</p>';
- }
+btnCompra.addEventListener('click', ()=>{
+    alert('Proximamente');
+})
